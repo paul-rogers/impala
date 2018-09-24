cf9f3e018530f4da8a05dcfaa65a42ec1a5a297f
Paul Rogers <progers@cloudera.com>
2018-09-24 12:57:51 -0700

    Add table stats for new table.

--- fe/src/test/java/org/apache/impala/planner/CardinalityTest.java
+++ fe/src/test/java/org/apache/impala/planner/CardinalityTest.java
@@ -75,23 +75,59 @@ public class CardinalityTest extends PlannerTestBase {
   @Test
   public void testNulls() {
     runTest("SELECT d FROM functional.nullrows", 26);
-    // a has unique values, so NDV = 26
+    // a has unique values, so NDV = 26, card = 26/26 = 1
     runTest("SELECT d FROM functional.nullrows WHERE a = 'x'", 1);
-    // f repeats for 5 rows, so NDV=6, 26/6 =~ 4
-    runTest("SELECT d FROM functional.nullrows WHERE f = 'x'", 4);
+    // f repeats for 5 rows, so NDV=7, 26/7 =~ 3
+    runTest("SELECT d FROM functional.nullrows WHERE f = 'x'", 3);
     // Revised use of nulls per IMPALA-7310
     // c is all nulls, NDV = 1, selectivity = 1/1, cardinality = 26
     runTest("SELECT d FROM functional.nullrows WHERE c = 'x'", 26);
+  }
+
+  @Test
+  public void testGroupBy() {
+    String baseStmt = "SELECT COUNT(*) " +
+                      "FROM functional.nullrows " +
+                      "GROUP BY ";
+    // NDV(a) = 26
+    runTest(baseStmt + "a", 26);
+    // f has NDV=3
+    runTest(baseStmt + "f", 8); // S.B. 7
+    // c is all nulls
+    runTest(baseStmt + "c", 1);
+    // NDV(a) * ndv(c) = 26 * 1 = 26
+    runTest(baseStmt + "a, c", 26);
+    // NDV(a) * ndv(f) = 26 * 3 = 78, capped at row count = 26
+    runTest(baseStmt + "a, f", 26);
+  }
+
+  /**
+   * Compute join cardinality using a table without
+   * stats. We estimate row count. Combine with an
+   * all-nulls column.
+   */
+
+  @Test
+  public void testJoinWithoutStats() {
     // NDV multiplied out on group by
     runTest("SELECT d FROM functional.alltypes, functional.nullrows", 7300 * 26);
+    // With that as the basis, add a GROUP BY
     String baseStmt = "SELECT COUNT(*) " +
                       "FROM functional.alltypes, functional.nullrows " +
                       "GROUP BY ";
+    // Unique values, one group per row
     runTest(baseStmt + "id", 7300);
+    // NDV(a) = 26
     runTest(baseStmt + "a", 26);
-    runTest(baseStmt + "f", 6);
+    // f has NDV=7
+    runTest(baseStmt + "f", 8); // S.b. 7
+    // c is all nulls
     runTest(baseStmt + "c", 1);
+    // NDV(a) = 26 * ndv(c) = 1
     runTest(baseStmt + "a, c", 26);
+    // NDV(a) = 26 * ndv(7) = 182
+    // Planner does not know that a determines f
+    runTest(baseStmt + "a, f", 208); // S.b. 182
   }
 
   /**
